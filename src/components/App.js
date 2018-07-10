d7ba175dbd272d9a4492b53b1e67bc0775fe882f
Adam Cohn <adamcohn88@gmail.com>
2018-07-10 15:31:42 -0400

    before merging branches

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import md5 from "js-md5";
-import { Route, Switch, withRouter } from "react-router-dom";
+import React from 'react';
+import md5 from 'js-md5';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
-import Nav from "./Nav";
-import RailsApi from "./RailsApi";
-import SearchContainer from "./LocationSearch/SearchContainer";
-import UserTrips from "./Trips/UserTrips";
-import Trip from "./Trips/Trip";
+import Nav from './Nav';
+import RailsApi from './RailsApi';
+import SearchContainer from './LocationSearch/SearchContainer';
+import UserTrips from './Trips/UserTrips';
+import Trip from './Trips/Trip';
 
-import NewTripContainer from "./TripCreation/NewTripContainer";
-import YelpSearchContainer from "./YelpSearch/YelpSearchContainer";
-import Error from "./Error";
+import NewTripContainer from './TripCreation/NewTripContainer';
+import YelpSearchContainer from './YelpSearch/YelpSearchContainer';
+import Error from './Error';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,7 +19,7 @@ class App extends React.Component {
       newTripLocation: [],
       trips: [],
       tripsLoaded: false,
-      error: false
+      error: false,
     };
   }
 
@@ -29,7 +29,7 @@ class App extends React.Component {
 
   updateTrips = () => {
     RailsApi.getTrips().then(trips =>
-      this.setState({ trips: trips, tripsLoaded: true, error: false })
+      this.setState({ trips: trips, tripsLoaded: true, error: false }),
     );
   };
 
@@ -40,46 +40,38 @@ class App extends React.Component {
         newTripLocation: {
           coords: userLocation.center,
           name: userLocation.text,
-          error: false
-        }
+          error: false,
+        },
       },
-      this.props.history.push("/new-trip")
+      this.props.history.push('/new-trip'),
     );
   };
 
   getWikiDataID = location => {
     let wikiDataId;
-    if (
-      location.properties.hasOwnProperty("wikidata") &&
-      location.properties.landmark !== true
-    ) {
+    if (location.properties.hasOwnProperty('wikidata') && location.properties.landmark !== true) {
       wikiDataId = location.properties.wikidata;
     } else {
-      wikiDataId = location.context.find(feature =>
-        feature.hasOwnProperty("wikidata")
-      ).wikidata;
+      wikiDataId = location.context.find(feature => feature.hasOwnProperty('wikidata')).wikidata;
     }
-    console.log(wikiDataId);
     fetch(
-      `https://cryptic-headland-94862.herokuapp.com/https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=${wikiDataId}&property=P18&format=json`
+      `https://cryptic-headland-94862.herokuapp.com/https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=${wikiDataId}&property=P18&format=json`,
     )
       .then(res => res.json())
-      .then(json =>
-        this.createImage(json.claims.P18["0"].mainsnak.datavalue.value)
-      )
+      .then(json => this.createImage(json.claims.P18['0'].mainsnak.datavalue.value))
       .catch(this.noImage);
   };
 
   noImage = () => {
-    this.setState({ image: "" });
+    this.setState({ image: '' });
   };
 
   createImage = name => {
-    const formattedName = name.split(" ").join("_");
+    const formattedName = name.split(' ').join('_');
     const mdSum = md5(formattedName).slice(0, 2);
     const link = `https://upload.wikimedia.org/wikipedia/commons/${mdSum.slice(
       0,
-      1
+      1,
     )}/${mdSum}/${formattedName}`;
     this.setState({ image: link });
   };
@@ -91,25 +83,23 @@ class App extends React.Component {
       address_latitude: this.state.newTripLocation.coords[1],
       address_longitude: this.state.newTripLocation.coords[0],
       image: this.state.image,
-      status: true
+      status: true,
     };
     RailsApi.postTrip(tripData).then(trip => {
       this.setState(
         {
           trips: [...this.state.trips, trip],
-          error: false
+          error: false,
         },
         () => {
-          this.props.history.push(
-            `trips/${this.state.trips[this.state.trips.length - 1].id}`
-          );
-        }
+          this.props.history.push(`trips/${this.state.trips[this.state.trips.length - 1].id}`);
+        },
       );
     });
   };
 
   locationError = () => {
-    this.setState({ error: true }), this.props.history.push("/home");
+    this.setState({ error: true }), this.props.history.push('/home');
   };
 
   render() {
@@ -128,10 +118,7 @@ class App extends React.Component {
               return (
                 <div>
                   {this.state.error === true && (
-                    <Error
-                      message={"Location could not be found, search again"}
-                      color={"red"}
-                    />
+                    <Error message={'Location could not be found, search again'} color={'red'} />
                   )}
                   <SearchContainer
                     saveLocation={this.setTripLocationState}
@@ -161,11 +148,7 @@ class App extends React.Component {
               return (
                 <div>
                   {this.state.tripsLoaded ? (
-                    <Trip
-                      trips={this.state.trips}
-                      {...props}
-                      updateTrips={this.updateTrips}
-                    />
+                    <Trip trips={this.state.trips} {...props} updateTrips={this.updateTrips} />
                   ) : (
                     <div />
                   )}
@@ -188,9 +171,7 @@ class App extends React.Component {
           <Route
             path="/add-venues"
             render={props => {
-              return (
-                <YelpSearchContainer location={this.state.newTripLocation} />
-              );
+              return <YelpSearchContainer location={this.state.newTripLocation} />;
             }}
           />
         </Switch>
